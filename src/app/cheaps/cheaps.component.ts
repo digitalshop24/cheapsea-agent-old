1f0ee35ce9da137743e732aeec688df46f415ffd
Konstantin D <konstantin.dks@gmail.com>
2018-04-17 00:01:33 +0300

    part

--- src/app/cheaps/cheaps.component.ts
+++ src/app/cheaps/cheaps.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {CheapsServise} from './cheaps.servise';
 import {Angular2TokenService} from 'angular2-token';
+import {UserData} from 'angular2-token/angular2-token.model';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-cheaps',
@@ -10,21 +12,40 @@ import {Angular2TokenService} from 'angular2-token';
 })
 export class CheapsComponent implements OnInit {
 
+  constructor(
+    private cheapsServise: CheapsServise,
+    private tokenService: Angular2TokenService,
+    private http: HttpClient
+  ) { }
+
   p: number = 1;
-
-  constructor(private cheapsServise: CheapsServise) { }
-
   cheaps = [];
+  user: any;
+
 
   ngOnInit() {
     document.body.classList.remove('login-bg');
-    this.cheapsServise.getCheaps(this.p).subscribe(
-      cheaps => {
-        console.log(cheaps);
-        this.cheaps = cheaps; },
-      error  => console.log(error)
-    );
+
+
+    this.tokenService.validateToken().subscribe((res: any) => {
+      this.user = res.json().data;
+
+      this.cheapsServise.getCheaps(this.p).subscribe(
+        cheaps => {
+          console.log(cheaps);
+          this.cheaps = cheaps; },
+        error  => console.log(error)
+      );
+    });
   }
+
+  getPlaceName(place_id) {
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?place_id=${place_id}&key=AIzaSyBEX35MB2wDC4dXdecw6SdFfu-_z-gD5rI`;
+    this.http.get(url).subscribe((res: any) => {
+      return res.results[0].formatted_address;
+    });
+  }
+
 }
 
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  // server_url: '//localhost:3000',
-  server_url: 'https://cheapsea.net',
+  server_url: '//localhost:3000',
+  // server_url: 'https://cheapsea.net',
   front_server_url: 'http://localhost:4200'
 };
